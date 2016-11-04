65a3c9de47b7ec6c4778d98287407fedbfb68292
Alexander Weber <web.alexander@web.de>
2016-11-04 15:25:40 +0100

    checks/list_spawning_mobs.lua updated

--- checks/list_spawning_mobs.lua
+++ checks/list_spawning_mobs.lua
@@ -1,3 +1,7 @@
-for name, def in pairs(mobs.spawning_mobs) do
-	print(name)
+if mobs and mobs.spawning_mobs then
+	for name, def in pairs(mobs.spawning_mobs) do
+		print(name)
+	end
+else
+	print("no mobs installed")
 end
