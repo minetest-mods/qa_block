a42d20d5466c2019b5d01023fe61476a008c709e
Wuzzy <almikes@aol.com>
2016-11-05 04:32:54 +0100

    Add checker for listing entities

---
+++ checks/list_entities.lua
@@ -0,0 +1,7 @@
+-- Lists all the registered entities (except builtin)
+for id, def in pairs(minetest.registered_entities) do
+	-- Ignore builtin entities
+	if string.sub(id, 1, 9) ~= "__builtin" then
+		print(id)
+	end
+end
